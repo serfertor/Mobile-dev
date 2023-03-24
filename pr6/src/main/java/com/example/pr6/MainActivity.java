c4c3c32484055a6c7dfcab0558ac9066346e85c3
George <serfertor@mail.ru>
2023-03-24 16:11:44 +0300

    Практика 6

---
+++ pr6/src/main/java/com/example/pr6/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.pr6;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity{
+
+    public MainActivity(){
+        super(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    protected void onResume() {
+        super.onResume();
+    }
+
+    protected void onStart() {
+        super.onStart();
+    }
+}
---
+++ pr6/src/main/java/com/example/pr6/SecretService.java
@@ -0,0 +1,67 @@
+package com.example.pr6;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class SecretService extends Service {
+    public SecretService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if ((flags & START_FLAG_RETRY) == 0) {
+            stopSelf();
+        }
+        return Service.START_NOT_STICKY;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+        params.gravity = Gravity.TOP | Gravity.RIGHT;
+        params.x = 0;
+        params.y = 0;
+        ConstraintLayout rootView = (ConstraintLayout) LayoutInflater.from(this).inflate(R.layout.service, null);
+        manager.addView(rootView, params);
+
+        rootView.findViewById(R.id.grab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK );
+                startActivity(intent);
+                stopForeground(true);
+                android.os.Process.killProcess(android.os.Process.myPid());
+                stopSelf();
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy(){
+        stopSelf();
+        super.onDestroy();
+    }
+
+}
